c4c8361272d64f974a90a459caae7e4910c9d4d8
Valerijxxx <drozdov17@icloud.com>
2024-06-27 13:28:45 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+document.addEventListener("DOMContentLoaded", () => {
+    const botUserName = 'Octopus_AIbot';
+    const telegramUrl = 'https://t.me/' + botUserName + '?start=start';
+
+    window.location.href = telegramUrl;
+});
+
